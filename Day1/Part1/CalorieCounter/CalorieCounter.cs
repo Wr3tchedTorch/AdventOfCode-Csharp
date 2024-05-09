33696329eeced75afdf996061b45572f33699c50
Eric Moura <ericmoura511@gmail.com>
2024-05-09 20:31:50 -0300

    Part 1 of Day 1 complete

---
+++ Day1/Part1/CalorieCounter/CalorieCounter.cs
@@ -0,0 +1,28 @@
+﻿namespace Domain;
+
+public class CalorieCounter
+{
+    string input;    
+    public CalorieCounter(String filePath)
+    {
+        input = new StreamReader(filePath).ReadToEnd();
+        input = input.Replace("\r", "");
+    }
+
+    public int GetHighestCalorieCount()
+    {
+        int highestCalorie = int.MinValue;
+        string[] elfsCalories = input.Split("\n\n");
+
+        for (int i = 0; i < elfsCalories.Length; i++)
+        {
+            int[] newElf = Array.ConvertAll(elfsCalories[i].Split("\n"), int.Parse);
+            
+            int caloriesSum = newElf.Sum();
+            
+            if (caloriesSum > highestCalorie) highestCalorie = caloriesSum;
+        }
+
+        return highestCalorie;
+    }
+}
---
+++ Day1/Part1/CalorieCounterTest/CalorieCounterTest.cs
@@ -0,0 +1,12 @@
+namespace CalorieCounterTest;
+using Domain;
+using FluentAssertions;
+
+public class CalorieCounterTest
+{
+    [Fact]
+    public void TestExampleInput() => new CalorieCounter("files/exampleInput.txt").GetHighestCalorieCount().Should().Be(24000);
+
+    [Fact]
+    public void TestPuzzleInput() => new CalorieCounter("files/puzzleInput.txt").GetHighestCalorieCount().Should().Be(66719);
+}
