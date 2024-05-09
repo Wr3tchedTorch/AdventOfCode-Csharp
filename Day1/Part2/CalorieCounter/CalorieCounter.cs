3ce6498dc907316c03d1a23c48e96f6a05f4c42f
Eric Moura <ericmoura511@gmail.com>
2024-05-09 20:52:40 -0300

    Part 2 of Day 1 complete

---
+++ Day1/Part2/CalorieCounter/CalorieCounter.cs
@@ -0,0 +1,59 @@
+﻿namespace Domain;
+
+public class CalorieCounter
+{
+    string input;
+    public CalorieCounter(String filePath)
+    {
+        input = new StreamReader(filePath).ReadToEnd();
+        input = input.Replace("\r", "");
+    }
+
+    public int GetHighestCalorieCount()
+    {
+        int highestCalorie = 0;
+        string[] elfsCalories = input.Split("\n\n");
+
+        for (int i = 0; i < elfsCalories.Length; i++)
+        {
+            int[] newElf = Array.ConvertAll(elfsCalories[i].Split("\n"), int.Parse);
+
+            int caloriesSum = newElf.Sum();
+
+            if (caloriesSum > highestCalorie) highestCalorie = caloriesSum;
+        }
+
+        return highestCalorie;
+    }
+
+    public int GetTopTreeCalorieCount()
+    {
+        int topOne = 0,
+            topTwo = 0,
+            topTree = 0;
+
+        string[] elfsCalories = input.Split("\n\n");
+
+        for (int i = 0; i < elfsCalories.Length; i++)
+        {
+            int[] newElf = Array.ConvertAll(elfsCalories[i].Split("\n"), int.Parse);
+
+            int caloriesSum = newElf.Sum();
+
+            if (caloriesSum > topOne)
+            {
+                topTree = topTwo;
+                topTwo = topOne;
+                topOne = caloriesSum;
+            }
+            else if (caloriesSum > topTwo)
+            {
+                topTree = topTwo;
+                topTwo = caloriesSum;
+            }
+            else if (caloriesSum > topTree) { topTree = caloriesSum; }
+        }
+
+        return topOne + topTree + topTwo;
+    }
+}
---
+++ Day1/Part2/CalorieCounterTest/CalorieCounterTest.cs
@@ -0,0 +1,17 @@
+namespace CalorieCounterTest;
+using Domain;
+using FluentAssertions;
+
+public class CalorieCounterTest
+{
+    [Fact]
+    public void TestExampleInput() => new CalorieCounter("files/exampleInput.txt").GetHighestCalorieCount().Should().Be(24000);
+
+    [Fact]
+    public void TestExampleInputTopTree() => new CalorieCounter("files/exampleInput.txt").GetTopTreeCalorieCount().Should().Be(45000);
+
+    [Fact]
+    public void TestPuzzleInput() => new CalorieCounter("files/puzzleInput.txt").GetHighestCalorieCount().Should().Be(66719);
+    [Fact]
+    public void TestPuzzleInputTopTree() => new CalorieCounter("files/puzzleInput.txt").GetTopTreeCalorieCount().Should().Be(198551);
+}
