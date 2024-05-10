06a4ce7ce11cc2938102a4f58a8e7af687c276ca
Eric Moura <124516716+Wr3tchedTorch@users.noreply.github.com>
2024-05-10 20:03:47 +0000

    puzzle input test passed

--- Day2/TestStrategyGuide/TestStrategyGuide.cs
+++ Day2/TestStrategyGuide/TestStrategyGuide.cs
@@ -5,9 +5,7 @@ using StrategyGuide;
 public class TestStrategyGuide
 {
     [Fact]
-    public void TestExampleInput()
-    {
-        StrategyGuide guideSheet = new StrategyGuide("./files/exampleInput.txt");
-        guideSheet.GetTotalScore().Should().Be(15);
-    }
+    public void TestExampleInput() => new StrategyGuide("./files/exampleInput.txt").GetTotalScore().Should().Be(15);
+    [Fact]
+    public void TestPuzzleInput() => new StrategyGuide("./files/puzzleInput.txt").GetTotalScore().Should().Be(13268);
 }
