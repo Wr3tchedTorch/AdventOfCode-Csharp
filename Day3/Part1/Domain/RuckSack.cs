4d3644a7d584f2a003da76fd102061aa1ea50d51
Eric Moura <ericmoura511@gmail.com>
2024-05-12 15:39:38 -0300

    Day 3 example input tests passed

---
+++ Day3/Part1/Domain/RuckSack.cs
@@ -0,0 +1,43 @@
+﻿namespace Domain;
+
+public class RuckSack
+{
+    string[] sacks;
+    public RuckSack(string filePath) => sacks = new StreamReader(filePath).ReadToEnd().Replace("\r", "").Split("\n");
+
+    public int PrioritySum()
+    {
+        int total = 0;
+        foreach (string sack in sacks)
+        {
+            total += GetSackPriority(sack);
+        }
+        return total;
+    }
+
+    private int GetSackPriority(string sack)
+    {
+        string firstSack = sack.Substring(0, sack.Length / 2);
+        string secondSack = sack.Substring(sack.Length / 2);
+
+        for (int i = 0; i < firstSack.Length; i++)
+        {
+            for (int j = 0; j < secondSack.Length; j++)
+            {
+                if (firstSack[i].Equals(secondSack[j]))
+                {
+                    return GetItemPriorityNumber(firstSack[i]);
+                }
+            }
+        }
+
+        throw new ArgumentException("each sack should contain exactly 1 matching item id, but none were found");
+    }
+
+    private int GetItemPriorityNumber(char itemId)
+    {
+        if (char.ToUpper(itemId).Equals(itemId)) return itemId - 38;
+
+        return itemId - 96;
+    }
+}
---
+++ Day3/Part1/RuckSackTest/RuckSackTest.cs
@@ -0,0 +1,13 @@
+using Domain;
+using FluentAssertions;
+
+namespace RuckSackTest;
+
+public class UnitTest1
+{
+    string baseUrl = Path.GetFullPath("./").Replace("\\bin\\Debug\\net8.0\\", "");
+
+    [Fact]
+    public void TestExampleInput() => new RuckSack($"{baseUrl}\\files\\exampleInput.txt").PrioritySum().Should().Be(157);
+    public void TestPuzzleInput() => new RuckSack($"{baseUrl}\\files\\puzzleInput.txt").PrioritySum().Should().Be(157);
+}
