7e7390064287aba70b8e374b2599d792ff2fc941
Eric Moura <ericmoura511@gmail.com>
2024-05-14 20:05:38 -0300

    program can now read columns from input and save them into Queues stored inside hashmaps

--- Day5/Part1/SupplyStacks/SupplyStacks.cs
+++ Day5/Part1/SupplyStacks/SupplyStacks.cs
@@ -1,9 +1,10 @@
 ﻿namespace SupplyStacks;
+using System.Collections;
 using System.Text.RegularExpressions;
 
 public class SupplyStacks
 {
-    string[,] cratesMap;
+    Dictionary<int, Queue<string>> stacksMap = new Dictionary<int, Queue<string>>();
     string[] steps;
     public SupplyStacks(string filePath)
     {
@@ -11,8 +12,32 @@ public class SupplyStacks
         string[] inputSplited = input.Split("\n\n");
 
         steps = Regex.Replace(inputSplited[1], "[^0-9.\n]", "").Split("\n");
-        int[] columns = Array.ConvertAll(Regex.Replace(inputSplited[0], "[^0-9.' ']", "").Trim().Split("   "), s => int.Parse(s));
-        System.Console.WriteLine(columns);
+        int stacksCount = Regex.Replace(inputSplited[0], "[^0-9.' ']", "").Trim().Split("   ").Length;
+
+        string[] cratesStack = Utils.ReplaceLastOccurrence(Regex.Replace(inputSplited[0], "[0-9]", ""), "\n", "").Split("\n");
+        cratesStack[cratesStack.Length - 1] = cratesStack[cratesStack.Length - 1].Trim();
+
+        SetStacksMap(stacksCount, cratesStack);
+    }
+
+    private void SetStacksMap(int stacksCount, string[] cratesStack)
+    {
+        for (int i = 1, offset = 0; i <= stacksCount; i++)
+        {
+            Queue<string> cratesInThisColumn = new Queue<string>();
+
+            foreach (string stack in cratesStack)
+            {
+                string crate = stack.Substring(offset * 3 + offset, 3).Trim();
+
+                if (string.IsNullOrEmpty(crate.Trim())) continue;
+
+                cratesInThisColumn.Enqueue(crate);
+            }
+
+            stacksMap.Add(i, cratesInThisColumn);
+            offset++;
+        }
     }
 
     public string GetTopCrates()
@@ -20,7 +45,7 @@ public class SupplyStacks
         throw new NotImplementedException();
     }
 
-    private void SortCrates()
+    private void SortStacks()
     {
         throw new NotFiniteNumberException();
     }
