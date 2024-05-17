24509d0a0f64bc95000480ac1e2cc6250890f4d8
Wr3tchedTorch <ericmoura511@gmail.com>
2024-05-17 17:22:28 -0300

    part 1 of day 6 being implemented

---
+++ Day6/Part1/Domain/TuningTrouble.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain;
+
+public class TuningTrouble
+{
+    string input;
+    public TuningTrouble(string inputPath) {
+        string input = new StreamReader(inputPath).ReadToEnd();
+        this.input = input;
+    }
+
+    public int GetStartOfPacketMarker() {
+        throw new NotImplementedException();
+    }
+
+    private int GetStartOfPacketMarker(int previousIndex) {
+        string sequence = input.Substring(previousIndex, previousIndex+4);
+        
+
+        return 0;
+    }
+}
---
+++ Day6/Part1/TuningTroubleTest/TuningTroubleTest.cs
@@ -0,0 +1,12 @@
+using Domain;
+using FluentAssertions;
+
+namespace TuningTroubleTest;
+
+public class TuningTroubleTest
+{
+    string basePath = Path.GetFullPath("./").Replace(@"\bin\Debug\net8.0\", "");
+
+    [Fact]
+    public void TestFirstSample() => new TuningTrouble($"{basePath}/files/firstSample.txt").GetStartOfPacketMarker().Should().Be(5);
+}
