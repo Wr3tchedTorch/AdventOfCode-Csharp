ebee067056fea2e9feafe26adb23964317198802
Wr3tchedTorch <ericmoura511@gmail.com>
2024-05-17 15:35:41 -0300

    tests for part 2 updated

--- Day5/Part2/SupplyStacksTest/SupplyStacksTest.cs
+++ Day5/Part2/SupplyStacksTest/SupplyStacksTest.cs
@@ -7,7 +7,7 @@ public class SupplyStacksTest
     string basePath = Path.GetFullPath("./").ToString().Replace("\\", "/").Replace("/bin/Debug/net8.0/", "");
 
     [Fact]
-    public void TestExampleInput() => new SupplyStacks($"{basePath}/files/exampleInput.txt").GetTopCrates().Should().Be("CMZ");
+    public void TestExampleInput() => new SupplyStacks($"{basePath}/files/exampleInput.txt").GetTopCrates().Should().Be("MCD");
     
     [Fact]
     public void TestPuzzleInput() => new SupplyStacks($"{basePath}/files/puzzleInput.txt").GetTopCrates().Should().Be("QMBMJDFTD");
