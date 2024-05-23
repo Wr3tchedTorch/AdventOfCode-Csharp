efe6d1eda34db36b6d5bc5228e0b7f5d96591196
Wr3tchedTorch <ericmoura511@gmail.com>
2024-05-23 16:02:29 -0300

    tests for example sample passed

--- Day7/Part1/FileSystem/FileSystem.cs
+++ Day7/Part1/FileSystem/FileSystem.cs
@@ -1,14 +1,80 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Reflection.PortableExecutable;
+using System.Text.RegularExpressions;
 
 public class FileSystem
 {
-    private string input;
+    private Dictionary<string, int> directories = [];
+    string input;
     public FileSystem(string filePath)
     {
         input = new StreamReader(filePath).ReadToEnd();
+        input = input.Replace("\r", "");
+        SetDirectories();
     }
 
-    public int GetSumOfDeletableFiles() {
-        throw new NotImplementedException();
+    private void AddDirectory(string path)
+    {
+        if (!directories.ContainsKey(path)) directories.Add(path, 0);
+    }
+
+    private void SetDirectories()
+    {
+        Stack<string> stack = [];
+        string currentPath = "";
+
+        foreach (string line in input.Split("\n"))
+        {
+            if (line == "$ ls" || line.Split()[0] == "dir") continue;
+
+            if (char.IsDigit(line.ToCharArray()[0]))
+            {
+                UpdateStackSize(stack, int.Parse(line.Split()[0]));
+                continue;
+            }
+
+            currentPath = RunCommand(line, stack, currentPath);
+        }
+    }
+
+    private void UpdateStackSize(Stack<string> stack, int size)
+    {
+        foreach (string path in stack)
+        {
+            directories[path] += size;
+        }
+    }
+
+    private string RunCommand(string command, Stack<string> stack, string currentPath)
+    {
+        if (command.Equals("$ cd .."))
+        {
+            stack.Pop();
+            return Regex.Replace(currentPath, @"\/(\w|\d)+$", "");
+        }
+
+        if (command.Equals("$ cd /"))
+        {
+            stack.Clear();
+            stack.Push("/");
+            AddDirectory("/");
+            return "/";
+        }
+
+        string newPath = Regex.Match(command, @"(?<=\$\scd\s).+").Value;
+        currentPath = currentPath == "/" ? $"/{newPath}" : $"{currentPath}/{newPath}";
+        stack.Push(currentPath);
+        
+        AddDirectory(currentPath);        
+        return currentPath;
+    }
+
+    public int GetSumOfDeletableFiles()
+    {
+        int total = 0;
+        foreach (int size in directories.Values)
+        {
+            if (size < 100000) total += size;
+        }
+        return total;
     }
 }
