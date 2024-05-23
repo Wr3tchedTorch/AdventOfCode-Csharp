27c0b362a6ba3015963949e0f6a056007b1c2078
Wr3tchedTorch <ericmoura511@gmail.com>
2024-05-23 16:05:53 -0300

    part 1 of day 7 complete

--- Day7/Part1/FileSystemTest/FileSystemTest.cs
+++ Day7/Part1/FileSystemTest/FileSystemTest.cs
@@ -7,4 +7,7 @@ public class FileSystemTest
 
     [Fact]
     public void TestExampleSample() => new FileSystem($"{basePath}/files/exampleSample.txt").GetSumOfDeletableFiles().Should().Be(95437);
+
+    [Fact]
+    public void TestPuzzleSample() => new FileSystem($"{basePath}/files/puzzleSample.txt").GetSumOfDeletableFiles().Should().Be(95437);
 }
