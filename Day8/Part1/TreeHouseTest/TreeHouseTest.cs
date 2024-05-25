305b77ec1e1252539b011ae36b805fd69725adb4
Wr3tchedTorch <ericmoura511@gmail.com>
2024-05-25 20:18:27 -0300

    tests for example sample passed

--- Day8/Part1/TreeHouseTest/TreeHouseTest.cs
+++ Day8/Part1/TreeHouseTest/TreeHouseTest.cs
@@ -8,4 +8,7 @@ public class TreeHouseTest
     private readonly string BasePath = Path.GetFullPath("./").Replace(@"\bin\Debug\net8.0", "");
     [Fact]
     public void TestExampleSample() => new TreeHouse($"{BasePath}/files/exampleSample.txt").GetNumberOfVisibleTrees().Should().Be(21);
+
+    [Fact]
+    public void TestPuzzleSample() => new TreeHouse($"{BasePath}/files/puzzleSample.txt").GetNumberOfVisibleTrees().Should().Be(21);
 }
