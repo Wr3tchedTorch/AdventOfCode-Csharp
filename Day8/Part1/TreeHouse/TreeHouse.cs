cc9ba5e97ac31ff129d6176d259aab42761e3bbc
Eric Moura <124516716+Wr3tchedTorch@users.noreply.github.com>
2024-05-27 17:52:37 +0000

    part 1 of day 8 complete

--- Day8/Part1/TreeHouse/TreeHouse.cs
+++ Day8/Part1/TreeHouse/TreeHouse.cs
@@ -3,7 +3,6 @@
 public class TreeHouse
 {
     private string[] inputLines { get; set; }
-    // private int[,] treeMap { get; set; }
     public TreeHouse(string filePath)
     {
         string input = new StreamReader(filePath).ReadToEnd();
@@ -12,33 +11,53 @@ public class TreeHouse
 
     public int GetNumberOfVisibleTrees()
     {
-        int rowsLength = inputLines.Length - 1;
-        int columnsLength = inputLines[0].Length - 1;
+        int rowsLength = inputLines.Length;
+        int columnsLength = inputLines[0].Length;
 
-        int total = (rowsLength * 2) + ((columnsLength * 2) - 2);
+        int total = 2 * (rowsLength + columnsLength) - 4;
         for (int row = 1; row < rowsLength - 1; row++)
         {
             for (int col = 1; col < columnsLength - 1; col++)
             {
-                int treeHeight = (int)char.GetNumericValue(inputLines[row].ToCharArray()[col]);
+                int treeHeight = GetTreeHeight(row, col);
                 if (IsVisible(treeHeight, row, col)) total++;
             }
         }
         return total;
     }
 
-    private bool IsVisible(int treeHeight, int targetRow, int targetCol)
+    private bool IsVisible(int treeHeight, int row, int col)
     {
-        for (int col = 0; col < inputLines[targetRow].Length-1; col++)
+        int left = col - 1, right = col + 1, top = row - 1, down = row + 1;
+        while (true)
         {
-            int otherTreeHeight = (int)char.GetNumericValue(inputLines[targetRow].ToCharArray()[col]);
-            if (treeHeight > otherTreeHeight) return true;
+            bool CanSeeThroughLeft = CanSeeThroughTree(treeHeight, row, left),
+            CanSeeThroughRight = CanSeeThroughTree(treeHeight, row, right),
+            CanSeeThroughTop = CanSeeThroughTree(treeHeight, top, col),
+            CanSeeThroughDown = CanSeeThroughTree(treeHeight, down, col);
+
+            if (CanSeeThroughLeft) left--;
+            if (left < 0) return true;
+
+            if (CanSeeThroughRight) right++;
+            if (right > inputLines[0].Length - 1) return true;
+
+            if (CanSeeThroughTop) top--;
+            if (top < 0) return true;
+
+            if (CanSeeThroughDown) down++;
+            if (down > inputLines.Length - 1) return true;
+
+            if (!CanSeeThroughLeft && !CanSeeThroughRight && !CanSeeThroughTop && !CanSeeThroughDown) return false;
         }
-        for (int row = 0; row < inputLines[row].Length-1; row++)
-        {
-            int otherTreeHeight = (int)char.GetNumericValue(inputLines[row].ToCharArray()[targetCol]);
-            if (treeHeight > otherTreeHeight) return true;
-        }
+    }
+
+    private int GetTreeHeight(int row, int col) => (int)char.GetNumericValue(inputLines[row].ToCharArray()[col]);
+
+    private bool CanSeeThroughTree(int treeHeight, int row, int col)
+    {
+        int otherTreeHeight = GetTreeHeight(row, col);
+        if (treeHeight > otherTreeHeight) return true;
 
         return false;
     }
--- Day8/Part1/TreeHouseTest/TreeHouseTest.cs
+++ Day8/Part1/TreeHouseTest/TreeHouseTest.cs
@@ -5,7 +5,7 @@ using TreeHouse;
 
 public class TreeHouseTest
 {
-    private readonly string BasePath = Path.GetFullPath("./").Replace(@"\bin\Debug\net8.0", "");
+    private readonly string BasePath = Path.GetFullPath("./").Replace(@"/bin/Debug/net8.0", "");
     [Fact]
     public void TestExampleSample() => new TreeHouse($"{BasePath}/files/exampleSample.txt").GetNumberOfVisibleTrees().Should().Be(21);
 
